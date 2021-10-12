f700b5330aeaa58647536c2ba3d7733762c6dd7e
juiceghost <jg@renegade.se>
2021-10-12 14:36:37 +0200

    at meltdown, part 2

---
+++ basics/main.py
@@ -0,0 +1,101 @@
+# Replace "type here" on line 2 with "Hello World!"
+print("Hello World!")
+
+# You can assign "Hello World!" to the variable below on line 3.
+
+my_text="Hello World!"
+print(my_text)
+
+# Type a couple of different values on line 9 inside the print function. Make sure they are separated by commas.    
+    
+print("Hello", "World", "!")
+#Type here. Assign a number to the variable: glass_of_water
+
+glass_of_water=3
+
+print("I drank", glass_of_water, "glasses of water today.")
+
+#Fill the print function so it prints glass_of_water
+
+glass_of_water=3
+
+glass_of_water += 1
+
+
+print(glass_of_water)
+
+# Assign an integer to the variable
+# Print the variable
+
+men_stepped_on_the_moon=12
+print(men_stepped_on_the_moon)
+
+#Type a couple of words or a short sentence for your variable, then print it.
+
+my_reason_for_coding="Can't do much else"
+print(my_reason_for_coding)
+
+#Assign a float with 2 decimals to the variable below. 
+#If you don't wan't to search the value you can check out Hint 1.
+
+global_mean_sea_level_2018=21
+
+#Type your code here.
+global_mean_sea_level_2018=21.36
+
+
+print(global_mean_sea_level_2018)
+
+
+#Assign True or False to the variable below then print it.
+
+staying_alive=True
+print(type(staying_alive))
+print(type(staying_alive) is bool)
+
+print(staying_alive)
+
+men_stepped_on_the_moon=12
+
+#Type your code here.
+
+answer_1=type(men_stepped_on_the_moon)
+
+
+print(answer_1)
+
+# <type 'int'>
+
+my_reason_for_coding="intergalactic travel"
+
+#Type your code here.
+
+answer_2=type(my_reason_for_coding)
+# <type 'str'>
+
+print (answer_2)
+
+staying_alive=True
+
+#Type your code here.
+
+
+answer_4=type(staying_alive)
+
+
+print(answer_4)
+
+# <type 'bool'>
+
+#my_grade variable is a string (because it's in quotes). 
+#On line 9, convert it to an integer.
+
+my_grade="10"
+
+answer_5=int(my_grade)
+
+print(type(my_grade))
+print(type(answer_5))
+print(answer_5)
+
+
--- meltdown-mitigation/conditionals.py
+++ meltdown-mitigation/conditionals.py
@@ -1,6 +1,10 @@
 """ Meltdown Mitigation exercise """
 
 
+from typing import Annotated
+from unittest.loader import defaultTestLoader
+
+
 def is_criticality_balanced(temperature, neutrons_emitted):
     """Verify criticality is balanced.
 
@@ -15,6 +19,19 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     """
     return temperature < 800 and neutrons_emitted > 500 and temperature * neutrons_emitted < 500000
 
+""" 
+print("below are some inputs and outputs from is_criticality_balanced")
+
+test_1 = is_criticality_balanced(750, 650)
+test_1_answer = True
+
+test_2 = is_criticality_balanced(1000, 800)
+test_2_answer = False
+
+print(f'Result of test 1 with data: (750, 650) was {test_1} but the correct answer is {test_1_answer}')
+print(f'Result of test 2 with data: (1000, 800) was {test_2} but the correct answer is {test_2_answer}')
+
+ """
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
     """Assess reactor efficiency zone.
@@ -26,18 +43,31 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 
     Efficiency can be grouped into 4 bands:
 
-    1. green -> efficiency of 80% or more,
-    2. orange -> efficiency of less than 80% but at least 60%,
-    3. red -> efficiency below 60%, but still 30% or more,
-    4. black ->  less than 30% efficient.
+    1. green -> efficiency of 80% or more,                     80..100
+    2. orange -> efficiency of less than 80% but at least 60%, 60..79 %
+    3. red -> efficiency below 60%, but still 30% or more,     30..59 %
+    4. black ->  less than 30% efficient. 0 < black < 29% |     0..29 %
 
     The percentage value is calculated as
     (generated power/ theoretical max power)*100
     where generated power = voltage * current
     """
 
-    pass
+    # 1. räkna ut generated power ( effekten vi genererar just i detta nu. I * U // voltage * current)
+    generated_power = voltage * current
+    # 2. räkna ut effektiviteten, dvs genererad effekt delat med teoretiskt möjlig maxeffekt
+    efficiency_percentage = (generated_power/ theoretical_max_power)*100
+    # 3. returnerna en av fyra strängar, beroende på intervallet hos efficiency_percentage
+    if efficiency_percentage < 30:
+        return 'black'
+    elif efficiency_percentage < 60:
+        return 'red'
+    elif efficiency_percentage < 80:
+        return 'orange'
+    else:
+        return 'green'
 
+#print(reactor_efficiency(10, 10, 400))
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
     """Assess and return status code for the reactor.
